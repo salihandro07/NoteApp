9610af10f009faea283ad3a13d16372627548448
salihandro07 <khamrayumovsalih2gmail.com>
2024-11-05 19:02:02 +0600

    OnBoard

---
+++ app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardFragment.kt
@@ -0,0 +1,53 @@
+package com.example.noteapp.ui.fragments.onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.example.noteapp.R
+import com.example.noteapp.databinding.FragmentOnBoardBinding
+import com.example.noteapp.ui.adapters.OnBoardAdapter
+
+class OnBoardFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnBoardBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentOnBoardBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+        setupListeners()
+    }
+
+    private fun initialize() {
+        binding.viewpager2.adapter = OnBoardAdapter(this)
+    }
+
+    private fun setupListeners() = with(binding.viewpager2) {
+        registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == 2){
+                    binding.txtSkip.visibility = View.INVISIBLE
+                }else{
+                    binding.txtSkip.visibility = View.VISIBLE
+                    binding.txtSkip.setOnClickListener {
+                        if(currentItem < 3){
+                            setCurrentItem(currentItem + 2, true)
+                        }
+                    }
+                }
+            }
+        })
+    }
+}
---
+++ app/src/main/java/com/example/noteapp/ui/fragments/onboard/OnBoardPagerFragment.kt
@@ -0,0 +1,52 @@
+package com.example.noteapp.ui.fragments.onboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.noteapp.R
+import com.example.noteapp.databinding.ActivityMainBinding
+import com.example.noteapp.databinding.FragmentOnBoardBinding
+import com.example.noteapp.databinding.FragmentOnBoardPagerBinding
+
+
+class OnBoardPagerFragment : Fragment() {
+
+    private lateinit var binding: FragmentOnBoardPagerBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        binding = FragmentOnBoardPagerBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialize()
+    }
+
+    private fun initialize() = with(binding) {
+        when(requireArguments().getInt(ARG_ONBOARD_POSITION)){
+            0 -> {
+                txtTitle.text = "Удобство"
+                txtBody.text = "Создавайте заметки в два клика! Записывайте мысли, идеи и важные задачи мгновенно."
+            }
+            1 -> {
+                txtTitle.text = "Организация"
+                txtBody.text = "Организуйте заметки по папкам и тегам. Легко находите нужную информацию в любое время."
+            }
+            2 -> {
+                txtTitle.text = "Синхронизация"
+                txtBody.text = "Синхронизация на всех устройствах. Доступ к записям в любое время и в любом месте."
+            }
+        }
+    }
+
+    companion object{
+        const val ARG_ONBOARD_POSITION = "onBoard"
+    }
+}
